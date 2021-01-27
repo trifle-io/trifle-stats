ba0d88ab2cef7f4f26a67383a0344866658352c8
Jozef Vaclavik <jozef@hey.com>
2021-01-27 07:12:49 +0100

    feat: cleanup operations a little

--- lib/trifle/ruby/operations/timeseries/increment.rb
+++ lib/trifle/ruby/operations/timeseries/increment.rb
@@ -4,13 +4,22 @@ module Trifle
   module Ruby
     module Operations
       module Timeseries
-        class Increment < Operation
+        class Increment
+          attr_reader :key, :values, :config
+
+          def initialize(**keywords)
+            @key = keywords.fetch(:key)
+            @at = keywords.fetch(:at)
+            @values = keywords.fetch(:values)
+            @config = keywords[:configuration] || Trifle::Ruby.config
+          end
+
           def perform
             config.ranges.map do |range|
               at = Nocturnal.new(@at).send("beginning_of_#{range}")
               config.driver.inc(
-                key: [@key, range, at.to_i].join(config.separator),
-                **@values
+                key: [key, range, at.to_i].join(config.separator),
+                **values
               )
             end
           end
--- lib/trifle/ruby/operations/timeseries/values.rb
+++ lib/trifle/ruby/operations/timeseries/values.rb
@@ -4,15 +4,25 @@ module Trifle
   module Ruby
     module Operations
       module Timeseries
-        class Values < Operation
+        class Values
+          attr_reader :key, :range, :config
+
+          def initialize(**keywords)
+            @key = keywords.fetch(:key)
+            @from = keywords.fetch(:from)
+            @to = keywords.fetch(:to)
+            @range = keywords.fetch(:range)
+            @config = keywords[:configuration] || Trifle::Ruby.config
+          end
+
           def timeline
-            Nocturnal.timeline(from: @from, to: @to, range: @range)
+            Nocturnal.timeline(from: @from, to: @to, range: range)
           end
 
           def perform
             timeline.map do |at|
               config.driver.get(
-                key: [@key, @range, at.to_i].join(config.separator)
+                key: [key, range, at.to_i].join(config.separator)
               )
             end
           end
