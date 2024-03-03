97769a4940bda18c1a567091ded8a8cf38e86725
Jozef Vaclavik <jozef@hey.com>
2024-03-03 08:24:36 +0400

    feat: formatter accepts path in a format method for better consistency

--- lib/trifle/stats/formatter/timeline.rb
+++ lib/trifle/stats/formatter/timeline.rb
@@ -6,19 +6,15 @@ module Trifle
       class Timeline
         include Trifle::Stats::Mixins::Packer
 
-        attr_reader :series, :path
+        attr_reader :series
 
-        def initialize(series:, path:)
+        def initialize(series:)
           @series = series
           @series[:values] = self.class.normalize(@series[:values])
-          @path = path
         end
 
-        def keys
-          @keys ||= path.split('.')
-        end
-
-        def format
+        def format(path:)
+          keys = path.split('.')
           series[:at].map.with_index do |at, i|
             value = series[:values][i].dig(*keys)
             block_given? ? yield(at, value) : [at, value.to_f]
