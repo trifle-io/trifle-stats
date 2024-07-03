ffecd79db986a1d86020a7d19cb68b029451d2ef
Jozef Vaclavik <jozef@hey.com>
2024-07-03 10:52:08 +0400

    feat: early return for empty series

--- lib/trifle/stats/aggregator/max.rb
+++ lib/trifle/stats/aggregator/max.rb
@@ -7,12 +7,16 @@ module Trifle
         Trifle::Stats::Series.register_aggregator(:max, self)
 
         def aggregate(series:, path:, slices: 1)
+          return [] if series[:at].empty?
+
           keys = path.split('.')
           result = series[:values].map do |data|
             data.dig(*keys)
           end
           sliced(result: result, slices: slices)
         end
+
+        private
 
         def sliced(result:, slices:)
           result[(result.count - (result.count / slices * slices))..].each_slice(result.count / slices).map do |slice|
--- lib/trifle/stats/formatter/timeline.rb
+++ lib/trifle/stats/formatter/timeline.rb
@@ -7,10 +7,14 @@ module Trifle
         Trifle::Stats::Series.register_formatter(:timeline, self)
 
         def format(series:, path:, slices: 1, &block)
+          return [] if series[:at].empty?
+
           keys = path.split('.')
           result = series[:at].zip(series[:values].map { |v| v.dig(*keys) })
           sliced(result: result, slices: slices, block: block)
         end
+
+        private
 
         def sliced(result:, slices:, block: nil)
           result[(result.count - (result.count / slices * slices))..].each_slice(result.count / slices).map do |slice|
